a96f07257bd8aaf70aa1f9edcc147b7a4fa9a83f
Guilherme <guilherme.passarinho@teamsoft.com.br>
2021-04-23 03:23:01 -0300

    initial commit

---
+++ youtube-clone/src/App.js
@@ -0,0 +1,11 @@
+import { Button } from "@material-ui/core";
+
+function App() {
+  return (
+    <Button variant="contained" color="primary">
+      Primary
+    </Button>
+  );
+}
+
+export default App;
